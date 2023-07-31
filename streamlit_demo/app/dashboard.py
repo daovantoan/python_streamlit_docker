7ad2a25704e869c8ebbe8ca3695e040eb6d0010c
Dao <daovantoan.tb@gmail.com>
2023-07-31 23:19:47 +0200

    Add files via upload

---
+++ streamlit_demo/app/dashboard.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('Dimensional Data Analysis')
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
